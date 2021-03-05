ff16f557a890c1fc0338259ee6df48bc4bb9ac99
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-03-05 12:59:42 +0800

    refactor props on Post

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -2,21 +2,24 @@ import React from "react";
 
 import classes from "./Post.module.scss";
 
-type PropsType = {
+type PostPropsType = {
     message: string;
     likesCount: number;
 };
 
-function Post({ message, likesCount }: PropsType) {
+const Post: React.FC<PostPropsType> = ({ message, likesCount }) => {
     return (
         <div className={classes.item}>
-            <img src="https://avatars.mds.yandex.net/get-zen_doc/1640581/pub_5e0497c82beb4900ad59a2d0_5e04996e028d6800ac20ec89/scale_1200" />
+            <img
+                src="https://avatars.mds.yandex.net/get-zen_doc/1640581/pub_5e0497c82beb4900ad59a2d0_5e04996e028d6800ac20ec89/scale_1200"
+                alt={"ava"}
+            />
             {message}
             <div>
                 <span>like</span> {likesCount}
             </div>
         </div>
     );
-}
+};
 
 export default Post;
