54cb827729f2fc0318fc2cdd7a81e5d7dc4f8fa8
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-03-14 23:00:27 +0800

    lesson 21: layout Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,24 @@ import React from "react";
 import classes from "./Dialogs.module.scss";
 
 function Dialogs() {
-    return <div>Dialogs</div>;
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogs_items}>
+                <div className={`${classes.item} ${classes.active}`}>
+                    Andrey
+                </div>
+                <div className={classes.item}>Oleg</div>
+                <div className={classes.item}>Sveta</div>
+                <div className={classes.item}>Valera</div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Yo</div>
+                <div className={classes.message}>What's up?</div>
+            </div>
+        </div>
+    );
 }
 
 export default Dialogs;
