93c3afdf272cd53c7eb35c1195c82784a16517e2
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-03-20 03:04:08 +0800

    lesson 25: map MyPosts and Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -32,16 +32,15 @@ function Dialogs() {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogs_items}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                {dialogsData.map((d) => (
+                    <DialogItem name={d.name} id={d.id} />
+                ))}
             </div>
 
             <div className={classes.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
+                {messagesData.map((m) => (
+                    <Message message={m.message} />
+                ))}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -27,14 +27,9 @@ function MyPosts() {
             </div>
 
             <div className={classes.posts}>
-                <Post
-                    message={postsData[0].message}
-                    likesCount={postsData[0].likesCount}
-                />
-                <Post
-                    message={postsData[1].message}
-                    likesCount={postsData[1].likesCount}
-                />
+                {postsData.map((p) => (
+                    <Post message={p.message} likesCount={p.likesCount} />
+                ))}
             </div>
         </div>
     );
