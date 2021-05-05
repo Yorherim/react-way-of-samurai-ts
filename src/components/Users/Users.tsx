088fe902a03d656918bf34846fb006b155e51209
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-05-05 23:56:55 +0800

    lesson 53: class User

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,58 +6,64 @@ import userPhoto from "../../assets/images/user.png";
 
 import { UsersPropsType } from "./UsersContainer";
 
-function Users({ users, follow, unfollow, setUsers }: UsersPropsType) {
-    const getUsers = () => {
-        if (users.length === 0) {
+class Users extends React.Component<UsersPropsType> {
+    constructor(props: UsersPropsType) {
+        super(props);
+        if (this.props.users.length === 0) {
             axios
                 .get("https://social-network.samuraijs.com/api/1.0/users")
-                .then((res) => setUsers(res.data.items))
+                .then((res) => this.props.setUsers(res.data.items))
                 .catch((err) => console.error(err));
         }
-    };
+    }
 
-    return (
-        <>
-            <button onClick={getUsers}>set users</button>
-            {users.map((user, i) => {
-                const followUser = () => follow(user.id);
-                const unfollowUser = () => unfollow(user.id);
+    render() {
+        const { users, follow, unfollow } = this.props;
 
-                return (
-                    <div key={`${user}_${i}`} className={classes.user}>
-                        <span>
-                            <div>
-                                <img
-                                    src={
-                                        user.photos.small
-                                            ? user.photos.small
-                                            : userPhoto
-                                    }
-                                    alt="avatar"
-                                />
-                            </div>
-                            <div>
-                                {user.followed ? (
-                                    <button onClick={unfollowUser}>
-                                        unfollow
-                                    </button>
-                                ) : (
-                                    <button onClick={followUser}>follow</button>
-                                )}
-                            </div>
-                        </span>
+        return (
+            <>
+                {users.map((user, i) => {
+                    const followUser = () => follow(user.id);
+                    const unfollowUser = () => unfollow(user.id);
 
-                        <span>
+                    return (
+                        <div key={`${user}_${i}`} className={classes.user}>
                             <span>
-                                <div>{user.name}</div>
-                                <div>{user.status}</div>
+                                <div>
+                                    <img
+                                        src={
+                                            user.photos.small
+                                                ? user.photos.small
+                                                : userPhoto
+                                        }
+                                        alt="avatar"
+                                    />
+                                </div>
+                                <div>
+                                    {user.followed ? (
+                                        <button onClick={unfollowUser}>
+                                            unfollow
+                                        </button>
+                                    ) : (
+                                        <button onClick={followUser}>
+                                            follow
+                                        </button>
+                                    )}
+                                </div>
                             </span>
-                        </span>
-                    </div>
-                );
-            })}
-        </>
-    );
+
+                            <span>
+                                <span>
+                                    <div>{user.name}</div>
+                                    <div>{user.status}</div>
+                                </span>
+                            </span>
+                        </div>
+                    );
+                })}
+            </>
+        );
+    }
 }
 
 export default Users;
